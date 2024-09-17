d5fafe28b5e06e8091b87c5be3dde03a1917df76
ModiSireesha <modisireesha09@gmail.com>
2024-09-17 15:47:48 +0530

    Added billing and payment components

---
+++ billing-payment/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BillingComponent } from './billing/billing.component';
+import { PaymentComponent } from './payment/payment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BillingComponent,
+    PaymentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
