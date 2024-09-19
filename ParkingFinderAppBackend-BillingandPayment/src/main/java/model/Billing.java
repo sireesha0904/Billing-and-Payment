392034c7e40f666872aeb5c5b52aa72a39b4ec24
ModiSireesha <modisireesha09@gmail.com>
2024-09-19 19:46:17 +0530

    updated model files

--- ParkingFinderAppBackend-BillingandPayment/src/main/java/model/Billing.java
+++ ParkingFinderAppBackend-BillingandPayment/src/main/java/model/Billing.java
@@ -1,5 +1,59 @@
 package model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Billing {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String vehicleNumber;
+    private double amount;
+    private LocalDateTime paymentDate;
+    
+    // Getters and Setters
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public LocalDateTime getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(LocalDateTime paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	
+	//constructor
+	public Billing(Long id, String vehicleNumber, double amount, LocalDateTime paymentDate) {
+		super();
+		this.id = id;
+		this.vehicleNumber = vehicleNumber;
+		this.amount = amount;
+		this.paymentDate = paymentDate;
+	}
+
+ 
+    
 }
--- ParkingFinderAppBackend-BillingandPayment/src/main/java/model/Receipt.java
+++ ParkingFinderAppBackend-BillingandPayment/src/main/java/model/Receipt.java
@@ -1,5 +1,64 @@
 package model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Receipt {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String receiptNumber;
+    private String vehicleNumber;
+    private double amount;
+    private LocalDateTime receiptDate;
+    
+    // Getters and Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getReceiptNumber() {
+		return receiptNumber;
+	}
+	public void setReceiptNumber(String receiptNumber) {
+		this.receiptNumber = receiptNumber;
+	}
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public LocalDateTime getReceiptDate() {
+		return receiptDate;
+	}
+	public void setReceiptDate(LocalDateTime receiptDate) {
+		this.receiptDate = receiptDate;
+	}
+	
+	//constructor
+	public Receipt(Long id, String receiptNumber, String vehicleNumber, double amount, LocalDateTime receiptDate) {
+		super();
+		this.id = id;
+		this.receiptNumber = receiptNumber;
+		this.vehicleNumber = vehicleNumber;
+		this.amount = amount;
+		this.receiptDate = receiptDate;
+	}    
+    
 }
