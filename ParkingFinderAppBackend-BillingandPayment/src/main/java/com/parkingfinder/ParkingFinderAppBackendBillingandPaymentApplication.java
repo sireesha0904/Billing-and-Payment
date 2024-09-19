ce43c84e7a6bc6a6f1a97d49b45a0021c8132997
ModiSireesha <modisireesha09@gmail.com>
2024-09-19 19:29:04 +0530

    Created Backend Part

---
+++ ParkingFinderAppBackend-BillingandPayment/src/main/java/com/parkingfinder/ParkingFinderAppBackendBillingandPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.parkingfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingFinderAppBackendBillingandPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingFinderAppBackendBillingandPaymentApplication.class, args);
+	}
+
+}
---
+++ ParkingFinderAppBackend-BillingandPayment/src/test/java/com/parkingfinder/ParkingFinderAppBackendBillingandPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkingfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingFinderAppBackendBillingandPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
