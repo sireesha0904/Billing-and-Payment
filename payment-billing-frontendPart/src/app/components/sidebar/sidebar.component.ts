acce9dba25a0a5af55a060164d4fa6c3604e46bc
Modi Sireesha <131472116+sireesha0904@users.noreply.github.com>
2025-02-12 22:40:41 +0530

    Update sidebar.component.ts

--- payment-billing-frontendPart/src/app/components/sidebar/sidebar.component.ts
+++ payment-billing-frontendPart/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RazorpayService } from '../../services/razorpay.service'; e
+import { RazorpayService } from '../../services/razorpay.service'; 
 
 @Component({
   selector: 'app-sidebar',
